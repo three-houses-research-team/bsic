3f434c8c98be2d0cd80115ff86421ef199b404ae
kolakcc <kolakcc@gmail.com>
2021-05-17 00:22:55 +0200

    Fix extractIndexNum check

--- gui/src/ScenarioGroupChooserFragment.kt
+++ gui/src/ScenarioGroupChooserFragment.kt
@@ -78,7 +78,7 @@ class ScenarioGroupChooserFragment : Fragment() {
 }
 
 fun File.containsData0() = File(this, "DATA0.bin").exists() && File(this, "DATA1.bin").exists()
-fun File.containsExtractedIndexNum() = File(this, "0").exists() && File(this, "0").exists()
+fun File.containsExtractedIndexNum() = File(this, "0").exists() && File(this, "1").exists()
 
 fun File.validateBaseDir(): Boolean = containsData0() || containsExtractedIndexNum()
 
